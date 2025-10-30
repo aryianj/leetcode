391e8eaff49667660e65aa0ba79e6713487d9327
Aryian Jones <acjones10@usfca.edu>
2025-10-29 19:03:41 -0700

    Time: 22 ms (13.67%) | Memory: 12.5 MB (13.44%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,26 +1,14 @@
 class Solution(object):
     def mergeAlternately(self, word1, word2):
-        word3 = ""
-        k = 0
-        w1 = False
-        w2 = False
-        for i in range(len(word1) + len(word2)):
-            if k < len(word1) and k < len(word2):
-                if i % 2 == 0: # if even
-                    word3 += word1[k]
-                else:
-                    word3 += word2[k]
-                    k += 1
-            else:
-                if k >= len(word1):
-                    w2 = True
-                else:
-                    w1 = True
-        if w1:
-            word3 += word1[k:]
-        else:
-            word3 += word2[k:]
-        return word3
+        i = 0
+        res = ""
+        while i < len(word1) or  i < len(word2):
+            if i < len(word1):
+                res += word1[i]
+            if i < len(word2):
+                res += word2[i]
+            i+=1
+        return res
         """
         :type word1: str
         :type word2: str
